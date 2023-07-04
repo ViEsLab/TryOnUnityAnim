1248df78a424eb171a1283d6a1487827e8b6e577
叶凌峰 <yelingfeng@xmfunny.com>
2023-07-04 17:58:25 +0800

    <feature> match target vault

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     private Animator anim;
+    private CharacterController characterController;
 
     private int speedRotateId = Animator.StringToHash("SpeedRotate");
     private int speedZId = Animator.StringToHash("SpeedZ");
-
     private int vaultId = Animator.StringToHash("Vault");
+    private int colliderId = Animator.StringToHash("Collider");
+    private Vector3 matchTarget = Vector3.zero;
 
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -21,15 +24,32 @@ public class Player : MonoBehaviour {
         anim.SetFloat(speedRotateId, Input.GetAxis("Horizontal") * 126);
 
         bool isVault = false;
-        if (anim.GetFloat(speedZId) > 3) {
+        if (anim.GetFloat(speedZId) > 3 && anim.GetCurrentAnimatorStateInfo(0).IsName("Locomotion")) {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position + Vector3.up * 0.3f, transform.forward, out hit, 2)) {
+            if (Physics.Raycast(transform.position + Vector3.up * 0.3f, transform.forward, out hit, 4f)) {
                 if (hit.collider.tag == "Obstacle") {
-                    isVault = true;
+                    if (hit.distance > 3) {
+                        Vector3 point = hit.point;
+                        point.y = hit.collider.transform.position.y + hit.collider.bounds.size.y + 0.07f;
+                        matchTarget = point;
+                        isVault = true;
+                    }
                 }
             }
         }
 
         anim.SetBool(vaultId, isVault);
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Vault") && anim.IsInTransition(0) == false) {
+            anim.MatchTarget(
+                matchTarget,
+                Quaternion.identity,
+                AvatarTarget.LeftHand,
+                new MatchTargetWeightMask(Vector3.one, 0),
+                0.32f,
+                0.4f);
+        }
+
+        characterController.enabled = anim.GetFloat(colliderId) < 0.05f;
     }
 }
